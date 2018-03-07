f4b80f82fdd73d03405e3c0e2d5f495cebb337b2
Saarah <saarahasad26@gmail.com>
2018-03-07 08:44:38 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+       
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">SA</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">About Us</a></li>
+            
+            </ul>
+            <form class="navbar-form navbar-right">
+                <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <div id="carousel-example-generic" class="carousel slide" style="margin-top:-20px;" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img style="width:100%" src="https://files2.porsche.com/filestore/image/multimedia/none/ww-rangeshotcountryselector-front/normal/6496c099-1896-11e8-bbc5-0019999cd470;s5/porsche-normal.jpg" alt="...">
+                <div class="carousel-caption">
+                    
+                </div>
+            </div>
+            
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <car-list></car-list>
+    `
+})
+export class AppComponent { }
---
+++ src/app/cars.ts
@@ -0,0 +1,8 @@
+export interface ICar{
+    id:number,
+    brand:string,
+    model:string,
+    year:number,
+    image:string,
+    description:string,
+}
